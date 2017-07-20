0e84b5596855ba14902638b4369be81b3cbae34d
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-07-20 19:57:09 +0200

    New master/detail project from xamarin forms template with PCL

---
+++ src/Greymind.Turns/Greymind.Turns.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Greymind.Turns.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Greymind.Turns.App());
+        }
+    }
+}
---
+++ src/Greymind.Turns/Greymind.Turns/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Greymind.Turns.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
