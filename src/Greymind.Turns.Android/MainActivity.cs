3733ffc75109c7f458d728e5063a86c65a108140
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-07-21 20:14:14 +0200

    ActionBar Tab

--- src/Greymind.Turns.Android/MainActivity.cs
+++ src/Greymind.Turns.Android/MainActivity.cs
@@ -4,15 +4,33 @@ using Android.OS;
 
 namespace Greymind.Turns.Android
 {
-    [Activity(Label = "Greymind.Turns.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Greymind Turns", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            SetContentView(Resource.Layout.Main);
+
+            var groupsTab = ActionBar.NewTab();
+            groupsTab.SetText("Groups");
+            groupsTab.TabSelected += (sender, args) =>
+            {
+
+            };
+
+            ActionBar.AddTab(groupsTab);
+
+            var activityTab = ActionBar.NewTab();
+            activityTab.SetText("Activity");
+            activityTab.TabSelected += (sender, args) =>
+            {
+
+            };
+
+            ActionBar.AddTab(activityTab);
         }
     }
 }
