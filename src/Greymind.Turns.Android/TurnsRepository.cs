c5e10ba6e7512735420ef7a13d87c49cfbfe76bc
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-07-23 14:07:56 +0200

    More mock data

--- src/Greymind.Turns.Android/TurnsRepository.cs
+++ src/Greymind.Turns.Android/TurnsRepository.cs
@@ -24,6 +24,16 @@ namespace Greymind.Turns.Android
             return people.ToArray();
         }
 
+        public void AddMembersToGroup(int groupId, params int[] personIds)
+        {
+            var people = this.people.Where(p => personIds.Contains(p.Id));
+
+            groups
+                .Single(g => g.Id == groupId)
+                .Members
+                .AddRange(people);
+        }
+
         public void InitializeMockData()
         {
             people = new List<Person>
@@ -32,24 +42,24 @@ namespace Greymind.Turns.Android
                 new Person{ Id = 1, Name = "Mavi" },
                 new Person{ Id = 2, Name = "Martin" },
                 new Person{ Id = 3, Name = "Matko" },
+                new Person{ Id = 4, Name = "Petra" },
             };
 
             groups = new List<Group>()
             {
                 new Group{ Id = 0, Name = "Burek" },
+                new Group{ Id = 1, Name = "Unicorns" },
             };
 
             activities = new List<Activity>
             {
                 new Activity{ Id = 0, Name = "Coffee", GroupId = 0},
                 new Activity{ Id = 1, Name = "Cake", GroupId = 0},
+                new Activity{ Id = 2, Name = "McDonald's", GroupId = 1},
             };
 
-            // Add members to group
-            groups
-                .Single(g => g.Name == "Burek")
-                .Members
-                .AddRange(people);
+            AddMembersToGroup(0, 0, 1, 2, 3);
+            AddMembersToGroup(1, 0, 4);
 
             // Populate group object based on group id
             activities
